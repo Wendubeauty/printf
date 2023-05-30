1160e311f8742b3b5357d2f9cb036a6c0b4951ef
joyedemevughe <joyedemevughe@gmail.com>
2023-05-30 13:17:52 -0700

    _printfc

--- _printf.c
+++ _printf.c
@@ -1,66 +1,95 @@
-#include <unistd.h>
-#include <stdarg.h>
 #include "main.h"
-
+#include <>
 /**
  * _printf - Produces output according to a format.
- * @format: Format string containing directives.
+ * @format: The format string.
  *
- * Return: Number of characters printed (excluding null byte).
+ * Return: The number of characters printed (excluding the null byte used to end output to strings).
  */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed_chars = 0;
-	char buffer[1024]; /* Buffer to store formatted output */
-	int buffer_index = 0;
-	int i;
+
+	if (format == NULL)
+		return (-1);
 
 	va_start(args, format);
-
-	for (i = 0; format && format[i]; i++)
-	{
-		if (format[i] == '%')
-		{
-			/* Handle conversion specifiers */
-			i++;
-			switch (format[i])
-			{
-				case 'c':
-					buffer[buffer_index++] = va_arg(args, int);
-					break;
-				case 's':
-					printed_chars += _putstr(&buffer[buffer_index]);
-					buffer_index = 0;
-					break;
-				case '%':
-					buffer[buffer_index++] = '%';
-					break;
-				default:
-					buffer[buffer_index++] = '%';
-					buffer[buffer_index++] = format[i];
-					break;
-			}
-		}
-		else
-		{
-			/* Store regular characters in buffer */
-			buffer[buffer_index++] = format[i];
-		}
-
-		/* Check if buffer is full and write to stdout */
-		if (buffer_index == 1024)
-		{
-			printed_chars += write(1, buffer, buffer_index);
-			buffer_index = 0;
-		}
-	}
-
-	/* Write remaining characters in buffer to stdout */
-	if (buffer_index > 0)
-		printed_chars += write(1, buffer, buffer_index);
-
+	printed_chars = format_parser(format, args);
 	va_end(args);
 
 	return (printed_chars);
+}
+
+/**
+ * format_parser - Parses the format string and processes the conversion specifiers.
+ * @format: The format string.
+ * @args: The variable arguments list.
+ *
+ * Return: The number of characters printed.
+ */
+int format_parser(const char *format, va_list args)
+{
+	int i = 0, printed_chars = 0;
+	char *str;
+
+	while (format && format[i])
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]);
+			printed_chars++;
+		}
+		else
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				_putchar(va_arg(args, int));
+				printed_chars++;
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				printed_chars += _puts(str);
+			}
+			else if (format[i] == '%')
+			{
+				_putchar('%');
+				printed_chars++;
+			}
+			else if (format[i] == '\0')
+				return (-1);
+			else
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				printed_chars += 2;
+			}
+		}
+		i++;
+	}
+
+	return (printed_chars);
+}
+
+/**
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ *
+ * Return: The number of characters printed.
+ */
+int _puts(char *str)
+{
+	int i = 0;
+
+	while (str && str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	return (i);
 }
